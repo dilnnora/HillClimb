93c876c0d92d7c51a8c80e38758f6926cdbd3079
Dilnora Matniyazova <dilnora.matniyazova@gmail.com>
2020-05-13 22:50:49 +0200

    Update tahta.java

--- TepeTirmanmasi/tahta.java
+++ TepeTirmanmasi/tahta.java
@@ -1,144 +1,118 @@
 package TepeTirmanmasi;
-
 import java.util.Random;
 
 public class tahta {
+    private int[][] board;
+    private int[] dizi;
+    private int h = 0;
 
-	private int[][] board;
-	private int[] dizi;
-	private int h = 0;
+    public tahta() {
+        board = new int[8][8];
+        dizi = new int[8];
+        h = 0;
+    }
 
-	
+    public void vezir_yerlestir(int gelen_dizi[]) { 
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                board[i][j] = 0;
 
-	public tahta() {
+        for (int a = 0; a < 8; a++)
+            board[gelen_dizi[a]][a] = 1;
 
-		board = new int[8][8];
-		dizi = new int[8];
-		h = 0;
+    }
 
-	}
+    public void dizi_yerlestir(int dizi2[]) { 
+        for (int a = 0; a < 8; a++)
+            dizi[a] = dizi2[a];
+    }
 
-	public void vezir_yerlestir(int gelen_dizi[]) { // tum vezirler dizide
-													// bulunan konumlara
-													// yerlesirler
+    public void yazdirBoard() { 
 
-		for (int i = 0; i < 8; i++)
-			for (int j = 0; j < 8; j++)
-				board[i][j] = 0;
+        System.out.println("----------TAHTA------------");
+        for (int i = 0; i < 8; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + "|");
+            }
 
-		for (int a = 0; a < 8; a++)
-			board[gelen_dizi[a]][a] = 1;
 
-	}
+            System.out.println("--------------------------");
 
-	public void dizi_yerlestir(int dizi2[]) { // disardan gelen diziden degerler
-												// atanir
+        }
 
-		for (int a = 0; a < 8; a++)
-			dizi[a] = dizi2[a];
+    }
 
-	}
+    public void setVezir(int satir, int sutun) {
 
-	public void yazdirBoard() { // konsol'a tahta yazdirilir
+        board[satir][sutun] = 1;
+    }
 
-		System.out.println("----------TAHTA------------");
+    public void kaldirVezir(int satir, int sutun) {
 
-		for (int i = 0; i < 8; i++) {
+        board[satir][sutun] = 0;
+    }
 
-			System.out.print("|");
+    public void setDizi(int satir, int sutun) {
 
-			for (int j = 0; j < 8; j++) {
+        dizi[sutun] = satir;
+    }
 
-				System.out.print(board[i][j] + "|");
+    public void yazdirDizi() {
 
-			}
+        System.out.println("-------------DIZI DEGERLERI------------------");
+        System.out.print("| ");
 
-			//System.out.println();
+        for (int i = 0; i < 8; i++) {
 
-			System.out.println("--------------------------");
+            System.out.print(dizi[i] + " | ");
+        }
+    }
 
-		}
+    public int bir_hucre(int i, int j) {
+        return board[i][j];
+    }
 
-	}
+    public void hesapla_heuristic() { 
 
-	public void setVezir(int satir, int sutun) {
+        for (int i = 0; i < 8; i++)
+            for (int j = i + 1; j < 8; j++) {
+                if (dizi[i] == dizi[j]) { /
+                    h++;
+                }
 
-		board[satir][sutun] = 1;
-	}
+                if (Math.abs(dizi[i] - dizi[j]) == Math.abs(i - j)) 
+                    h++;
+            }
+    }
 
-	public void kaldirVezir(int satir, int sutun) {
+    public void vezirAtamasi() { 
 
-		board[satir][sutun] = 0;
-	}
+        Random rnd = new Random();
+        int sayi;
+        for (int i = 0; i < 8; i++) {
+            sayi = rnd.nextInt(8);
+            setVezir(sayi, i);
+            setDizi(sayi, i);
+        }
 
-	public void setDizi(int satir, int sutun) {
+        sifirlaHeuristic();
+        hesapla_heuristic();
+    }
 
-		dizi[sutun] = satir;
-	}
+    public int getHeuristic() {
 
-	public void yazdirDizi() {
+        return h;
+    }
 
-		System.out.println("-------------DIZI DEGERLERI------------------");
+    public void sifirlaHeuristic() {
 
-		System.out.print("| ");
+        h = 0;
+    }
 
-		for (int i = 0; i < 8; i++) {
+    public int[] getDizi() {
 
-			System.out.print(dizi[i] + " | ");
-		}
-
-	}
-
-	public int bir_hucre(int i, int j) {
-
-		return board[i][j];
-	}
-
-	public void hesapla_heuristic() { // tum ataklar (heuristic) hesaplanir
-
-		for (int i = 0; i < 8; i++)
-			for (int j = i + 1; j < 8; j++) {
-				if (dizi[i] == dizi[j]) { // ayný satirda ise
-					h++;
-				}
-
-				if (Math.abs(dizi[i] - dizi[j]) == Math.abs(i - j)) // diagonal
-																	// icin
-					h++;
-			}
-
-	}
-
-	public void vezirAtamasi() { // tum vezirler rasgele atanirlar
-
-		Random rnd = new Random();
-		int sayi;
-
-		for (int i = 0; i < 8; i++) {
-			sayi = rnd.nextInt(8);
-			setVezir(sayi, i);
-
-			setDizi(sayi, i);
-
-		}
-
-		sifirlaHeuristic();
-		hesapla_heuristic();
-	}
-
-	public int getHeuristic() {
-
-		return h;
-	}
-
-	public void sifirlaHeuristic() {
-
-		h = 0;
-	}
-
-	public int[] getDizi() {
-
-		return dizi;
-	}
+        return dizi;
+    }
 
 }
